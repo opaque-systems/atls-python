6ccb033f9668c23a87450701bfadb9213c2b2273
Hernan <hernan.gatta@opaque.co>
2023-08-17 16:03:59 +0000

    pyatls: add list of well-known public AAS JKUs

--- python-package/src/pyatls/validators/__init__.py
+++ python-package/src/pyatls/validators/__init__.py
@@ -1,8 +1,10 @@
+from pyatls.validators.az_aas import AZ_AAS_GLOBAL_JKUS
 from pyatls.validators.az_aas_aci_validator import AzAasAciValidator
 from pyatls.validators.az_aas_cvm_validator import AzAasCvmValidator
 from pyatls.validators.validator import SecurityWarning, Validator
 
 __all__ = [
+    "AZ_AAS_GLOBAL_JKUS",
     "AzAasAciValidator",
     "AzAasCvmValidator",
     "SecurityWarning",
---
+++ python-package/src/pyatls/validators/az_aas.py
@@ -0,0 +1,8 @@
+from typing import List
+
+AZ_AAS_GLOBAL_JKUS: List[str] = [
+    "https://sharedcus.cus.attest.azure.net/certs",
+    "https://sharedeus.eus.attest.azure.net/certs",
+    "https://sharedeus2.eus2.attest.azure.net/certs",
+    "https://shareduks.uks.attest.azure.net/certs",
+]
