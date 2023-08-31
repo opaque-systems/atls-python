1103263772053a0bbdf9b6f85f014471cd95d464
Hernan <hernan.gatta@opaque.co>
2023-08-31 20:24:05 +0000

    pyatls: improve sample

--- python-package/sample/connect_aci.py
+++ python-package/sample/connect_aci.py
@@ -20,12 +20,16 @@
 
 import argparse
 import ast
+import warnings
 from typing import List, Mapping, Optional
 
-import requests
+import urllib3
 from atls import ATLSContext, HTTPAConnection
 from atls.utils.requests import HTTPAAdapter
+from atls.utils.urllib3 import extract_from_urllib3, inject_into_urllib3
+from atls.validators import Validator
 from atls.validators.azure.aas import AciValidator
+from cryptography.x509.oid import ObjectIdentifier
 
 # Parse arguments
 parser = argparse.ArgumentParser()
@@ -82,13 +86,42 @@ parser.add_argument(
 )
 
 parser.add_argument(
+    "--loops",
+    default=1,
+    help="number of times to perform the request to evaluate the impact of "
+    "connection pooling (default: 1)",
+)
+
+parser.add_argument(
+    "--use-injection",
+    action="store_true",
+    help="inject aTLS support under the urllib3 library to automatically "
+    "upgade all HTTPS connections into HTTP/aTLS (default: false)",
+)
+
+parser.add_argument(
     "--use-requests",
     action="store_true",
     help="use the requests library with the HTTPS/aTLS adapater (default: "
     "false)",
 )
 
+parser.add_argument(
+    "--insecure",
+    action="store_true",
+    help="disable attestation (testing only) (default false)",
+)
+
 args = parser.parse_args()
+
+if args.insecure and (args.policy or args.jku):
+    raise Exception(
+        "Cannot specify --policy and/or --jku alongside --insecure"
+    )
+
+loops: int = int(args.loops)
+if loops == 0 or loops < 0:
+    raise ValueError(f"Invalid loop count: {loops}")
 
 policy_files: Optional[List[str]] = args.policy
 jkus: Optional[List[str]] = args.jku
@@ -101,14 +134,43 @@ if policy_files is not None:
         with open(filepath) as f:
             policies.append(f.read())
 
-# Set up the Azure AAS ACI validator:
+
+class NullValidator(Validator):
+    """
+    A validator that accepts any evidence, effectively bypassing attestation.
+
+    This can be useful to evaluate the overhead of the attestation process. For
+    example, when using AAS, the endpoint may be too far away from where this
+    script is running and therefore incur significant latency. To test that
+    hypothesis, it may be valuable to momentarily disable attestation for the
+    sake of debugging.
+
+    Do not use in production.
+    """
+
+    @staticmethod
+    def accepts(_oid: ObjectIdentifier) -> bool:
+        return True
+
+    def validate(
+        self, _document: bytes, _public_key: bytes, _nonce: bytes
+    ) -> bool:
+        warnings.warn("Skipping attestation...")
+        return True
+
+
+# Set up the Azure AAS ACI validator, unless it has been explicitly disabled:
 # - The policies array carries all allowed CCE policies, or none if the policy
 #   should be ignored.
 #
 # - The JKUs array carries all allowed JWKS URLs, or none if the JKU claim in
 #   the AAS JWT token sent by the server during the aTLS handshake should not
 #   be checked.
-validator = AciValidator(policies=policies, jkus=jkus)
+validator: Validator
+if args.insecure:
+    validator = NullValidator()
+else:
+    validator = AciValidator(policies=policies, jkus=jkus)
 
 # Parse provided headers, if any.
 headers: Mapping[str, str] = {}
@@ -127,27 +189,50 @@ def use_direct() -> None:
     # validator (only one need succeed).
     ctx = ATLSContext([validator])
 
-    # Set up the HTTP request machinery using the aTLS context.
-    conn = HTTPAConnection(args.server, ctx, args.port)
+    # Purposefully create a new connection per loop to incur the cost of
+    # attestation to highlight the added value of connection pooling as
+    # provided by the urllib3 and requests libraries.
+    for _ in range(loops):
+        # Set up the HTTP request machinery using the aTLS context.
+        conn = HTTPAConnection(args.server, ctx, args.port)
 
-    # Send the HTTP request, and read and print the response in the usual way.
-    conn.request(
-        args.method,
-        args.url,
-        body,
-        headers,
-    )
+        # Send the HTTP request, and read and print the response in the usual
+        # way.
+        conn.request(args.method, args.url, body, headers)
 
-    response = conn.getresponse()
-    code = response.getcode()
+        response = conn.getresponse()
 
-    print(f"Status: {code}")
-    print(f"Response: {response.read().decode()}")
+        print(f"Status: {response.status}")
+        print(f"Response: {response.data.decode()}")
 
-    conn.close()
+        conn.close()
+
+
+def use_injection() -> None:
+    # Replace urllib3's default HTTPSConnection class with HTTPAConnection.
+    inject_into_urllib3([validator])
+
+    for _ in range(loops):
+        # The rest of urllib3's usage is as usual.
+        response = urllib3.request(
+            "POST",
+            f"https://{args.server}:{args.port}{args.url}",
+            body=body,
+            headers=headers,
+        )
+
+        print(f"Status: {response.status}")
+        print(f"Response: {response.data.decode()}")
+
+    # Restore the default HTTPSConnection class.
+    extract_from_urllib3()
 
 
 def use_requests() -> None:
+    # Note that this is an optional dependency of PyAtls since it is not
+    # strictly required for its operation.
+    import requests
+
     session = requests.Session()
 
     # Mount the HTTP/aTLS adapter such that any URL whose scheme is httpa://
@@ -155,22 +240,25 @@ def use_requests() -> None:
     # connection with the server.
     session.mount("httpa://", HTTPAAdapter([validator]))
 
-    # The rest of the usage of the requests library is as usual. Do remember to
-    # use session.request from the session object that has the mounted adapter,
-    # not requests.request, since that's the global request function and has
-    # therefore no knowledge of the adapter.
-    response = session.request(
-        args.method,
-        f"httpa://{args.server}:{args.port}{args.url}",
-        data=body,
-        headers=headers,
-    )
+    for _ in range(loops):
+        # The rest of the usage of the requests library is as usual. Do
+        # remember to use session.request from the session object that has the
+        # mounted adapter, not requests.request, since that's the global
+        # request function and has therefore no knowledge of the adapter.
+        response = session.request(
+            args.method,
+            f"httpa://{args.server}:{args.port}{args.url}",
+            data=body,
+            headers=headers,
+        )
 
-    print(f"Status: {response.status_code}")
-    print(f"Response: {response.text}")
+        print(f"Status: {response.status_code}")
+        print(f"Response: {response.text}")
 
 
 if args.use_requests:
     use_requests()
+elif args.use_injection:
+    use_injection()
 else:
     use_direct()
